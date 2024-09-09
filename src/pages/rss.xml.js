19ae91ba95855a113ec19d80b720691bc36fe89d
Derrick Dryer <contact@derrickdryer.com>
2024-09-09 11:02:40 -0400

    Convert back to old RSS feed. Will revamp later

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -12,6 +12,5 @@ export async function GET(context) {
       ...post.data,
       link: `/blog/${post.slug}/`,
     })),
-    customData: `<atom:link href="${context.site}/rss.xml" rel="self" type="application/rss+xml" />`,
   });
 }
