c46c323301fa02ca80f05725401ec78908131c40
Derrick Dryer <contact@derrickdryer.com>
2024-09-09 10:54:01 -0400

    Add atom:link for better to add compatibility with more rss readers. May revert if there are issues.

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,17 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { siteConfig } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { siteConfig } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: siteConfig.title,
-		description: siteConfig.description,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
-	});
+  const posts = await getCollection("blog");
+  return rss({
+    title: siteConfig.title,
+    description: siteConfig.description,
+    site: context.site,
+    items: posts.map((post) => ({
+      ...post.data,
+      link: `/blog/${post.slug}/`,
+    })),
+    customData: `<atom:link href="${context.site}/rss.xml" rel="self" type="application/rss+xml" />`,
+  });
 }
